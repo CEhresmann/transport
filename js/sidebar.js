30ec225e4e339996adce6380623da7aff81bc145
Тимофей <Trubinov04@gmail.com>
2025-06-28 21:12:23 +0200

    some scripts

---
+++ js/sidebar.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebarContainer = document.getElementById("sidebar-container");
+
+    if (sidebarContainer) {
+        fetch("../includes/sidebar.html")
+            .then((res) => res.text())
+            .then((data) => {
+                sidebarContainer.innerHTML = data;
+            })
+            .catch((err) => {
+                console.error("Ошибка загрузки sidebar.html", err);
+            });
+    }
+});
