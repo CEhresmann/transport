27dd739648438acbdba78399a943e34dedf01c08
Тимофей <Trubinov04@gmail.com>
2025-06-28 21:28:56 +0200

    боковое меню

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const sidebarContainer = document.getElementById("sidebar-container");
 
     if (sidebarContainer) {
-        fetch("../includes/sidebar.html")
+        fetch("../../includes/sidebar.html")
             .then((res) => res.text())
             .then((data) => {
                 sidebarContainer.innerHTML = data;
