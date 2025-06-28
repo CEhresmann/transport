9aee99e94f49af86e91c22319384ea080d1297c9
Тимофей <Trubinov04@gmail.com>
2025-06-28 22:47:16 +0200

    pizdec

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("sidebar-container");
     if (container) {
-        fetch("includes/sidebar.html")
+        fetch("../includes/sidebar.html")
             .then((res) => res.text())
             .then((html) => {
                 container.innerHTML = html;
