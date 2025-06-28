5873be189a3e5d70ba41e6557a8a332aa220a6c6
Тимофей <Trubinov04@gmail.com>
2025-06-28 22:34:20 +0200

    стилька навалил

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sidebarContainer = document.getElementById("sidebar-container");
-
-    if (sidebarContainer) {
-        fetch("../../includes/sidebar.html")
+    const container = document.getElementById("sidebar-container");
+    if (container) {
+        fetch("includes/sidebar.html")
             .then((res) => res.text())
-            .then((data) => {
-                sidebarContainer.innerHTML = data;
+            .then((html) => {
+                container.innerHTML = html;
             })
-            .catch((err) => {
-                console.error("Ошибка загрузки sidebar.html", err);
-            });
+            .catch((err) => console.error("Ошибка загрузки sidebar:", err));
     }
 });
